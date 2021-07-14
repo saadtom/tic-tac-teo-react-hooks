48deff93087d4ab71113d956cd15abd7db9ae2c0
Tommy Tomaye <tommytomaye2018@gmail.com>
2021-07-14 14:08:13 -0700

    update board

--- src/game-board.js
+++ src/game-board.js
@@ -1,27 +1,28 @@
 import React, { useReducer } from "react";
-import calculateWinner from './calculate-winner';
+import calculateWinner from "./calculate-winner";
 
 function getStatus(squares, xIsNext) {
-    const winner = calculateWinner(squares)
-    if (winner) {
-        return `Winner: ${winner}`
-    } else if (squares.every(Boolean)) {
-        return `Tie`
-    } else {
-        return `Next Player: ${xIsNext ? 'X' : '0'}`
-    }
+  const winner = calculateWinner(squares);
+  if (winner) {
+    return `Winner: ${winner}`;
+  } else if (squares.every(Boolean)) {
+    return `Tie`;
+  } else {
+    return `Next Player: ${xIsNext ? "X" : "0"}`;
+  }
 }
 
 function gameReducer(state, action) {
   const { squares, xIsNext } = state;
   switch (action.type) {
     case "SELECT_SQUARE": {
-      const square = action;
+      const { square } = action;
       const winner = calculateWinner(squares);
       if (winner || squares[square]) {
         return state;
       }
       const squaresCopy = [...squares];
+      squaresCopy[square] = xIsNext ? 'X' : '0'
       return {
         squares: squaresCopy,
         xIsNext: !xIsNext,
@@ -42,40 +43,40 @@ const Board = () => {
   });
 
   const { squares, xIsNext } = state;
-
-  function renderSquare(index) {
+  const renderSquare = (index) => {
     return (
-        <button className='square' onClick={() => selectSquare(index)}>
-            {squares[index]}
-        </button>
-    )
-  }
+      <button className="square" onClick={() => selectSquare(index)}>
+        {squares[index]}
+      </button>
+    );
+  };
 
-  function selectSquare(square) {
-      dispatch({type: 'SELECT_SQUARE', square})
-  }
+  const selectSquare = (square) => {
+      console.log(square);
+    dispatch({ type: "SELECT_SQUARE", square });
+  };
 
-  const status = getStatus(squares, xIsNext)
+  const status = getStatus(squares, xIsNext);
 
   return (
-      <>
-        <div className='status'>{status}</div>
-        <div className='board-row'>
-            {renderSquare(0)}
-            {renderSquare(1)}
-            {renderSquare(2)}
-        </div>
-        <div className='board-row'>
-            {renderSquare(3)}
-            {renderSquare(4)}
-            {renderSquare(5)}
-        </div>
-        <div className='board-row'>
-            {renderSquare(6)}
-            {renderSquare(7)}
-            {renderSquare(8)}
-        </div>
-      </>
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </>
   );
 };
 
